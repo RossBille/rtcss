b18eeeb4ef42855677e5e3571f22b7413d661404
Ross Bille <rossjbille@gmail.com>
2017-06-01 21:31:26 +1000

    adds babel config for signalling server

--- rtcss-signalling-server-socket.io/src/server.js
+++ rtcss-signalling-server-socket.io/src/server.js
@@ -1,27 +1,23 @@
 /**
  * Created by rossbille on 21/04/2015.
  */
-var info = 'info';
+import nodeStatic from "node-static";
+import http from "http";
+import IO from "socket.io";
 
-function log(level, message) {
-    console.log(message);
-}
-var nodeStatic = require('node-static');
-var http = require('http');
-nodeStatic.mime.types.ts = "application/x-typescript";
-var file = new (nodeStatic.Server)();
-var port = 2013;
+const file = new (nodeStatic.Server)();
+const port = 2013;
 
-var app = http.createServer(function (req, res) {
+const app = http.createServer(function (req, res) {
     file.serve(req, res);
 }).listen(port);
-log(info, 'Started server, listening on ' + port);
+console.log('Started server, listening on ' + port);
 
-var clients = [];
+const clients = [];
 
 Array.prototype.remove = function (obj) {
-    var i = this.indexOf(obj);
-    if (i != -1) {
+    const i = this.indexOf(obj);
+    if (i !== -1) {
         this.splice(i, 1);
     }
 };
@@ -30,22 +26,20 @@ function prefixId(id) {
     return 'id_' + id;
 }
 function removeIdPrefix(id) {
-    var newId = id.substring(3);
-    return newId;
+    return id.substring(3);
 }
 
-
-var io = require('socket.io').listen(app);
+const io = IO.listen(app);
 io.sockets.on('connection', function (socket) {
     socket.id = prefixId(socket.id);
 
-    var thisSocket = socket.id;
+    const thisSocket = socket.id;
 
-    log(info, 'New connection, socket.id: ' + thisSocket);
+    console.log('New connection, socket.id: ' + thisSocket);
 
     //handle client disconnecting
     socket.on('disconnect', function () {
-        log(info, 'Closed connection, socket.id: ' + thisSocket);
+        console.log('Closed connection, socket.id: ' + thisSocket);
         //remove from servers list of clients
         clients.remove(thisSocket);
 
@@ -55,13 +49,13 @@ io.sockets.on('connection', function (socket) {
 
     //handle client messages
     socket.on('message', function (message) {
-        log(info, "Got message from: " + thisSocket + ". To: " + message.toClient);
-        var toClient = removeIdPrefix(message.toClient);
-        var connected = io.sockets.connected;
+        console.log("Got message from: " + thisSocket + ". To: " + message.toClient);
+        const toClient = removeIdPrefix(message.toClient);
+        const connected = io.sockets.connected;
         if (toClient in connected) {
             connected[toClient].emit('message', message);
         } else {
-            log("error", "client: " + toClient + " is not connected");
+            console.log("client: " + toClient + " is not connected");
             clients.remove(toClient);
         }
     });
@@ -78,5 +72,5 @@ io.sockets.on('connection', function (socket) {
 
     //add this client to the servers list of clients
     clients.push(thisSocket);
-    log(info, 'Current clients: ' + clients);
+    console.log('Current clients: ' + clients);
 });
